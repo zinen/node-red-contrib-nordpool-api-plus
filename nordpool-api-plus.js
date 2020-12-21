5afc733dac2183ebd1024d136a54bf7c574f43a0
zinen <zinen@users.noreply.github.com>
2020-12-21 22:11:07 +0100

    Removed redundant code added to fix #6
    Since `nordpool@v2.1.0` connection errors no longer crashes node-red and thus making this changed made to fix #6. This code is therefore reverted some parts back.

--- nordpool-api-plus.js
+++ nordpool-api-plus.js
@@ -34,48 +34,40 @@ module.exports = function (RED) {
         currency: CURRENCY, // can also be 'DKK', 'NOK', 'SEK'
         date: date
       }
-      // Check for access to nordpool website
-      require('dns').resolve('www.nordpoolspot.com', function (error) {
+      const prices = new nordpool.Prices()
+      const pricesOut = []
+      prices.hourly(opts, function (error, results) {
         if (error) {
-          node.status({ fill: 'red', text: 'Error while requesting data' })
+          node.status({ fill: 'red', text: 'Error getting data' })
           done(error)
-        } else {
-          const prices = new nordpool.Prices()
-          const pricesOut = []
-          prices.hourly(opts, function (error, results) {
-            if (error) {
-              node.status({ fill: 'red', text: 'Error while receiving data' })
-              done(error)
-              return
-            }
-            // Check if data is received from API call
-            if (results.length === 0) {
-              // It seems that all areas support EUR, but not other currencies
-              if (opts.currency !== 'EUR') {
-                node.status({ fill: 'yellow', text: 'No data. Some areas only support EUR as currency' })
-              } else {
-                node.status({ fill: 'yellow', text: 'No data found' })
-              }
-              msg.payload = null
-              send(msg)
-              done()
-              return
-            }
-            for (var i = 0; i < results.length; i++) {
-              const values = {
-                timestamp: results[i].date.tz('Europe/Oslo'), // Convert moment object to native js time and date (UTC)
-                price: results[i].value,
-                currency: opts.currency,
-                area: AREA
-              }
-              pricesOut.push(values)
-            }
-            msg.payload = pricesOut
-            send(msg)
-            node.status({ fill: '', text: 'Done' })
-            done()
-          })
+          return
         }
+        // Check if data is received from API call
+        if (results.length === 0) {
+          // It seems that all areas support EUR, but not other currencies
+          if (opts.currency !== 'EUR') {
+            node.status({ fill: 'yellow', text: 'No data. Some areas only support EUR as currency' })
+          } else {
+            node.status({ fill: 'yellow', text: 'No data found' })
+          }
+          msg.payload = null
+          send(msg)
+          done()
+          return
+        }
+        for (var i = 0; i < results.length; i++) {
+          const values = {
+            timestamp: results[i].date.tz('Europe/Oslo'), // Convert moment object to native js time and date (UTC)
+            price: results[i].value,
+            currency: opts.currency,
+            area: AREA
+          }
+          pricesOut.push(values)
+        }
+        msg.payload = pricesOut
+        send(msg)
+        node.status({ fill: '', text: 'Done' })
+        done()
       })
     })
   }
