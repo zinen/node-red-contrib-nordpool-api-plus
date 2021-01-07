3ad7c1928879ac4d319b2ec97ad87d2a2f8bddce
zinen <zinen@users.noreply.github.com>
2021-01-07 10:25:41 +0100

    Adopt to nordpool v3.0.0

--- nordpool-api-plus.js
+++ nordpool-api-plus.js
@@ -1,20 +1,15 @@
 module.exports = function (RED) {
   function nordpoolAPIPlus (config) {
     RED.nodes.createNode(this, config)
-
     // The nodes config:
-
     this.area = config.area
     this.currency = config.currency
     this.date = config.date
     const node = this
     const nordpool = require('nordpool')
-
     this.status({ text: 'Ready' })
-
-    node.on('input', function (msg, send, done) {
+    node.on('input', async function (msg, send, done) {
       this.status({ fill: 'blue', shape: 'dot', text: 'Getting prices' })
-
       const AREA = msg.area || node.area || 'Oslo'
       const CURRENCY = msg.currency || node.currency || 'EUR'
       let date
@@ -29,46 +24,45 @@ module.exports = function (RED) {
       } else {
         date = new Date().toISOString()
       }
-      var opts = {
+      const opts = {
         area: AREA, // See https://www.nordpoolgroup.com/Market-data1/#/nordic/map
         currency: CURRENCY, // can also be 'DKK', 'NOK', 'SEK'
         date: date
       }
       const prices = new nordpool.Prices()
-      const pricesOut = []
-      prices.hourly(opts, function (error, results) {
-        if (error) {
-          node.status({ fill: 'red', text: 'Error getting data' })
-          done(error)
-          return
+      let results
+      try {
+        results = await prices.hourly(opts)
+      } catch (error) {
+        node.status({ fill: 'red', text: 'Error getting data' })
+        done(error)
+      }
+      // Check if data is received from API call
+      if (results.length === 0) {
+        // It seems that all areas support EUR, but not other currencies
+        if (opts.currency !== 'EUR') {
+          node.status({ fill: 'yellow', text: 'No data at date. Some areas only support EUR as currency' })
+        } else {
+          node.status({ fill: 'yellow', text: 'No data found for the requested date' })
         }
-        // Check if data is received from API call
-        if (results.length === 0) {
-          // It seems that all areas support EUR, but not other currencies
-          if (opts.currency !== 'EUR') {
-            node.status({ fill: 'yellow', text: 'No data. Some areas only support EUR as currency' })
-          } else {
-            node.status({ fill: 'yellow', text: 'No data found' })
-          }
-          msg.payload = null
-          send(msg)
-          done()
-          return
-        }
-        for (var i = 0; i < results.length; i++) {
-          const values = {
-            timestamp: results[i].date.tz('Europe/Oslo'), // Convert moment object to native js time and date (UTC)
-            price: results[i].value,
-            currency: opts.currency,
-            area: AREA
-          }
-          pricesOut.push(values)
-        }
-        msg.payload = pricesOut
+        msg.payload = null
         send(msg)
-        node.status({ fill: '', text: 'Done' })
         done()
-      })
+        return
+      }
+      msg.payload = []
+      for (var i = 0; i < results.length; i++) {
+        const values = {
+          timestamp: results[i].date,
+          price: results[i].value,
+          currency: opts.currency,
+          area: AREA
+        }
+        msg.payload.push(values)
+      }
+      send(msg)
+      node.status({ fill: '', text: 'Done' })
+      done()
     })
   }
   RED.nodes.registerType('nordpool-api-plus', nordpoolAPIPlus)
